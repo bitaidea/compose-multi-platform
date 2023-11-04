d1f89f5ed572876898e8c93cc71db8654fcbaa14
Abbasali Mollahoseini <abbasali88m@yahoo.com>
2023-11-04 10:12:14 +0330

    localized

--- shared/src/desktopMain/kotlin/main.desktop.kt
+++ shared/src/desktopMain/kotlin/main.desktop.kt
@@ -9,6 +9,8 @@ import com.myapplication.MowjDatabase
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.okhttp.OkHttp
+import java.util.Locale
+import java.util.ResourceBundle
 import java.util.concurrent.TimeUnit
 
 
@@ -39,3 +41,12 @@ actual fun httpClient(config: HttpClientConfig<*>.()-> Unit) = HttpClient(OkHttp
 @Composable
 actual fun font(name: String, res: String, weight: FontWeight, style: FontStyle): Font =
   Font("font/$res.ttf", weight, style)
+
+
+actual object Strings {
+    actual fun get(id: String): String {
+        val locale = Locale("en") // Set the appropriate locale (e.g., based on user preferences)
+        val bundle = ResourceBundle.getBundle("string", locale)
+        return  bundle.getString(id)
+    }
+}
